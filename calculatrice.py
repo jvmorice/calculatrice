3f21210486714878a551f439e0ad65102a7b2bf4
Julie Morice <julie@morice.pro>
2023-04-16 15:08:09 +0200

    Fix some possible syntax errors when entering brackets.

--- calculatrice.py
+++ calculatrice.py
@@ -67,6 +67,28 @@ class Calculatrice():
         self.entry.grid(row=row, column=col, columnspan=5,
                         sticky='we', padx=2, pady=2)
 
+    def allclear_button(self, symbol, row, col):
+        """Create the button clearing the entry field."""
+        self.button = tk.Button(self.frame, text=symbol, bg='#CFCFCF',
+                                relief=tk.FLAT, font=self.button_font,
+                                command=self.click_allclear_button)
+        self.button.grid(row=row, column=col, sticky='wens', padx=2, pady=2)
+
+    def remove_button(self, symbol, row, col):
+        """Create the backspace button."""
+        self.button = tk.Button(self.frame, text=symbol, bg='#CFCFCF',
+                                relief=tk.FLAT, font=self.button_font,
+                                command=self.click_remove_button)
+        self.button.grid(row=row, column=col, sticky='wens', padx=2, pady=2)
+
+    def bracket_button(self, symbol, row, col):
+        """Create a bracket button."""
+        self.button = tk.Button(self.frame, text=symbol, bg='#CFCFCF',
+                                relief=tk.FLAT, font=self.button_font,
+                                command=lambda:
+                                    self.click_bracket_button(symbol))
+        self.button.grid(row=row, column=col, sticky='wens', padx=2, pady=2)
+
     def number_button(self, symbol, row, col):
         """Create a button entering a digit or a dot."""
         self.button = tk.Button(self.frame, text=symbol, bg='#FFFFFF',
@@ -83,14 +105,6 @@ class Calculatrice():
                                     self.click_operation_button(symbol))
         self.button.grid(row=row, column=col, sticky='wens', padx=2, pady=2)
 
-    def bracket_button(self, symbol, row, col):
-        """Create a bracket button."""
-        self.button = tk.Button(self.frame, text=symbol, bg='#CFCFCF',
-                                relief=tk.FLAT, font=self.button_font,
-                                command=lambda:
-                                    self.click_bracket_button(symbol))
-        self.button.grid(row=row, column=col, sticky='wens', padx=2, pady=2)
-
     def result_button(self, symbol, row, col):
         """Create the button calculating the entered expression."""
         self.button = tk.Button(self.frame, text=symbol, bg='#4890E1',
@@ -98,39 +112,56 @@ class Calculatrice():
                                 command=self.calculate)
         self.button.grid(row=row, column=col, sticky='wens', padx=2, pady=2)
 
-    def remove_button(self, symbol, row, col):
-        """Create the backspace button."""
-        self.button = tk.Button(self.frame, text=symbol, bg='#CFCFCF',
-                                relief=tk.FLAT, font=self.button_font,
-                                command=self.click_remove_button)
-        self.button.grid(row=row, column=col, sticky='wens', padx=2, pady=2)
-
-    def allclear_button(self, symbol, row, col):
-        """Create the button clearing the entry field."""
-        self.button = tk.Button(self.frame, text=symbol, bg='#CFCFCF',
-                                relief=tk.FLAT, font=self.button_font,
-                                command=self.click_allclear_button)
-        self.button.grid(row=row, column=col, sticky='wens', padx=2, pady=2)
-
 # ------------------------------- PROGRAM LOGIC -------------------------------
 
     def click_allclear_button(self):
-        """Clear the entry field."""
+        """Clear the entry field and place an initial zero in it."""
         self.entry.delete(0, tk.END)
         self.entry.insert(0, '0')
 
     def click_remove_button(self):
-        """Remove the last entered character."""
+        """Remove the last entered character.
+
+        If all characters are removed, an initial zero is placed in the field.
+        """
         self.string = self.entry.get()
+        self.string = self.string[:-1]
         self.entry.delete(0, tk.END)
-        self.entry.insert(0, self.string[0:-1])
-        if len(self.string) == 1:
+        self.entry.insert(0, self.string)
+        if len(self.string) == 0:
             self.entry.insert(0, '0')
+
+    def click_bracket_button(self, symbol):
+        """Enter brackets.
+
+        If an opening bracket is entered after a digit, the multiplication sign
+        is placed before the bracket.
+        If an opening bracket is entered after the dot, the dot is deleted and
+        the multiplication sign is placed between the digit and the bracket.
+        If a closing bracket is entered right after the opening one,
+        the input of the closing bracket is blocked.
+        A closing bracket can be entered only if the number of opening brackets
+        does not exceed the number of closing brackets, including the
+        entered one.
+        """
+        self.string = self.entry.get()
+        if self.string == '0' and symbol == '(':
+            self.string = self.string[1:] + symbol
+        elif self.string[-1].isdigit() and symbol == '(':
+            self.string = self.string + '*' + symbol
+        elif self.string[-1] == '.' and symbol == '(':
+            self.string = self.string[:-1] + '*' + symbol
+        elif self.string[-1] == '(' and symbol == ')':
+            self.string = self.string
+        elif self.string.count('(') > self.string.count(')') and symbol == ')':
+            self.string = self.string + symbol
+        self.entry.delete(0, tk.END)
+        self.entry.insert(tk.END, self.string)
 
     def click_number_button(self, symbol):
         """Enter digits.
 
-        The starting zero is replaced by the entered digit. The starting zero
+        The initial zero is replaced by the entered digit. The initial zero
         is saved, if the second character is a dot. If a dot is entered
         after an operation character, a zero is inserted before it, for example
         '5*.2' is replaced by '5*0.2'.
@@ -143,26 +174,6 @@ class Calculatrice():
         self.entry.delete(0, tk.END)
         self.entry.insert(tk.END, self.string + symbol)
 
-    def click_bracket_button(self, symbol):
-        """Enter brackets.
-
-        An opening bracket is entered only after the operation character,
-        or after another opening bracket or at the beginning, otherwise an error
-        message is displayed. A closing bracket is entered only after
-        a number character and only if the number of opening brackets does not
-        exceed the number of closing brackets, including the entered one.
-        """
-        self.string = self.entry.get()
-        if self.string == '0' and symbol == '(':
-            self.string = self.string[1:]
-            self.entry.delete(0, tk.END)
-            self.entry.insert(tk.END, symbol)
-        elif self.string[-1] not in '+-*/(' and symbol == '(' :
-            mb.showerror('Attention!', 'Enter an operation symbol (+, -, * or /)')
-        elif self.string.count('(') <= self.string.count(')') and symbol == ')':
-            mb.showerror('Attention!', 'Excessive closing bracket')
-        else:
-            self.entry.insert(tk.END, symbol)
 
     def click_operation_button(self, symbol):
         """Enter operation characters.
